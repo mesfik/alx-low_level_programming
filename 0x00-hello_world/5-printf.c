006d83152ba0df968e0a3a3aa92230a3fbe6a47c
mesfik <fikreamesfin@gmail.com>
2022-09-10 20:07:55 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
 *
 * return: Always 0 (Success)
 */
-int main(void)
+int main(void) /*void main function*/
 {
 
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0) ;
+return (0);
 
 }

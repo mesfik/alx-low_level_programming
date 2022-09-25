6ead5b5639e72cef4e34e33b32cbc9e592ecd2f6
mesfik <fikreamesfin@gmail.com>
2022-09-25 20:19:30 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	i = 97;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+

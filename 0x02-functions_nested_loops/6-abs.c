cb3f03d6ec5e74eb4f163c60ec4901ae40fa932d
mesfik <fikreamesfin@gmail.com>
2022-10-03 18:18:44 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - a fuction that prints the absolute value
+ * @a: intiger input
+ * Return: absolute value of a
+ */
+int _abs(int a)
+{
+	return (a * ((a > 0) - (a < 0)));
+}
+

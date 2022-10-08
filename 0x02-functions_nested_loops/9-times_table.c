08e205c421a3cd1837f5ba2bb89ce5e820c4e0da
mesfik <fikreamesfin@gmail.com>
2022-10-08 18:19:12 +0000

    times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - prints the nine times table
+ *
+ */
+void times_table(void)
+{
+	int t = 9;
+
+	int r, c, m;
+
+	for (r = 0; r <= t; r++)
+	{
+		for (c = 0; c <= t; c++)
+		{
+			m = r * c;
+			if (m / 10)
+				_putchar(48 + (m / 10));
+			else if (c != 0)
+				_putchar(' ');
+			_putchar(48 + (m % 10);
+
+			if (c != t)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			}
+			_putchar('\n');
+			}
+			}
+

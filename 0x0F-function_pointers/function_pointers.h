98ecd29f7225db91a91b56574d1b17241fbf85ef
mesfik <fikreamesfin@gmail.com>
2022-10-14 13:52:12 +0000

    prints name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+
+#endif

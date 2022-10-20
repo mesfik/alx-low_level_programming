17f329bfebce04ee03ad4d5e9066c927778df5a9
mesfik <fikreamesfin@gmail.com>
2022-10-20 18:01:30 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two intiger
+ *
+ * @a: intiger to swap
+ * @b: intiger to swap
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	m = *a;
+	*a = *b;
+	*b = n;
+}

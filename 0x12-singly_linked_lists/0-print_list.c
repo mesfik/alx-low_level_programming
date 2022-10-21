7258c5df513f5774f66a2a0c4d5f4fa01a05b782
mesfik <fikreamesfin@gmail.com>
2022-10-21 20:11:52 +0000

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - print elements of the list
+ *
+ * @h: linked lists
+ *
+ * Return: size
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count;
+
+	count = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] %s\n", "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

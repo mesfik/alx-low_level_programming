189406ced533d26a5740d4bb8697c88bee547dd5
mesfik <fikreamesfin@gmail.com>
2022-10-25 20:50:33 +0000

    list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nOfnodes = 0;
+	size_t a = 0;
 
-	for (nOfnodes = 0; h; nOfnodes++)
+	for (a = 0; h; a++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (nOfnodes);
+	return (a);
 }

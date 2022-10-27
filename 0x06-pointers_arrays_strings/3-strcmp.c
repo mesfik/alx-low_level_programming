070be11295d8fb399116363e00bf5ea9deb3fe3e
mesfik <fikreamesfin@gmail.com>
2022-10-27 11:47:46 +0000

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ *
+ * @s1: entry string name
+ * @s2: entry string name
+ *
+ * Return: 0 or s1 greater than s2 or s2 greater than s1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}

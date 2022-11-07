6ce86dd81e8d884c1ed2589f63dd3bf5953514ff
mesfik <fikreamesfin@gmail.com>
2022-11-07 20:45:41 +0000

    length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,15 +9,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
-
-	for (i = 0; s[i]; i++)
+	unsigned int i, j, k, flag;
+	
+	k = 0;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
 			if (s[i] == accept[j])
-				break;
-		if (!accept[i])
-			break;
+			{
+				k++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (k);
+		}
 	}
-	return (i + 1);
+	return (0);
 }

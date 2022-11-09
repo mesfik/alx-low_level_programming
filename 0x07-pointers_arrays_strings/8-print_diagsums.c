986d36bea458ca09bd4def027294d5d48565e9cb
mesfik <fikreamesfin@gmail.com>
2022-11-09 11:13:04 +0000

    diasum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,16 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i;
+	unsigned int sum1, sum2;
 
-	for (i = 0; i < size * size; i++)
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size; i++)
 	{
-		if (i % (size + 1) == 0)
-			sum1 += a[i];
-		if (i % (size - 1) && i != 0 && i < (size * size - 1))
-			sum2 += a[i];
+		sum1 += a[(size * i) + i];
+		sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }

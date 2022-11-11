d50a149ad091dfca384be96401048d19695b942e
mesfik <fikreamesfin@gmail.com>
2022-11-11 19:27:20 +0000

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - function main entry
+ *
+ * @argc: argument counting
+ * @argv: argument variable
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	       printf("%s\n", argv[i]);
+	return (0);
+}

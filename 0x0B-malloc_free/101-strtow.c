25bb4bb515a69086d92fd277ee2f33eaae01a045
mesfik <fikreamesfin@gmail.com>
2022-11-14 21:03:00 +0000

    argument

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * strtow - function that splits a string into words
+ *
+ * @str: string pointer
+ *
+ * Return: pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+

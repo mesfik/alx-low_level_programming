96fc6e15a28aa9e51f45aeeb603a73bfeb4ac1fc
mesfik <fikreamesfin@gmail.com>
2022-11-14 18:28:37 +0000

    conc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,9 +21,9 @@ char *str_concat(char *s1, char *s2)
 	if (conc == NULL)
 		return (NULL);
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 	for (i = 0; s1[i]; i++)
 	{
 		conc[i] = s1[i];

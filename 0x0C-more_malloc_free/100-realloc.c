0c58d8707c64eb69d493a4b93ca902f537260955
mesfik <fikreamesfin@gmail.com>
2022-12-11 14:53:50 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
-		malloc(new_size);
+		return (malloc(new_size));
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);

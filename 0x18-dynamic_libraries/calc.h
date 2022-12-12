bdb7fee61ef602ac57c53d4cd84cb75df18da5ec
mesfik <fikreamesfin@gmail.com>
2022-12-12 20:06:44 +0000

    opration

---
+++ 0x18-dynamic_libraries/calc.h
@@ -0,0 +1,13 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int divs(int a, int b);
+int modl(int a, int b);
+
+#endif

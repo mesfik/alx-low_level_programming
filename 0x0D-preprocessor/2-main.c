7f2d3a53fcdca1548fee24a86eddd52eb88afb18
mesfik <fikreamesfin@gmail.com>
2022-12-13 20:02:33 +0000

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ * Description: name of the file
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
